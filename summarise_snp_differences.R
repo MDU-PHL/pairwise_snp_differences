cf3b40e2252b78712e652786ca17f859905c81d8
Anders Goncalves da Silva <andersgs@gmail.com>
2015-08-27 10:37:33 +1000

    Building the script by adding the main summary function.

--- summarise_snp_differences.R
+++ summarise_snp_differences.R
@@ -36,4 +36,141 @@
 ################################################################################
 
 
+################################################################################
+# If not running off the command line, change these parameters to point the 
+# approriate files, e.g.:
+#   cat = '/home/user/cat.csv'
+
+cat_file = NULL
+
+# Only one of these files needs to be specified. If both are specified, the
+# diff file will have precedence
+seq_file = NULL
+diff_file = NULL
+
+################################################################################
+
+
+
+################################################################################
+# The main function
+# 
+summ_distances <- function(dist_obj, categories){
+  #dist_obj is a distance object produced by using the dist.dna() function of ape
+  #categories is a data.frame with two columns:
+  #   - seq_id: that matches the sequence ids in dist_obj
+  #   - groups: that assigns the individual seq_ids to a group
+  dat <- as.matrix(dist_obj)
+  if(class(dist_obj) != 'dist') {
+    stop("dist_obj is not an object of type dist! 
+         Please use dist.dna() to create a distance object first.")
+  }
+  
+  if(!is.data.frame(categories)){
+    stop("categories must be a data.frame! 
+         Please create a data.frame with the metadata first.")
+  }
+  
+  if(ncol(categories) > 2) {
+    warning("Number of colums in categories is >2, 
+            taking the first two columns only")
+    categories <- categories[,c(1,2)]
+  }
+  
+  if(!all(sort(names(categories)) == sort(c("seq_id", "groups")))) {
+    warning("The columns of categories do not have names this function 
+            recognizes. It will assume that the first column contains seq_ids, 
+            and the second column the relevant categories.")
+    names(categories) <- c("seq_id", "categories")
+  }
+  
+  taxa <- unique(as.character(categories[,'groups']))
+  n_taxa <- length(taxa)
+  total_comp <- (n_taxa^2 + n_taxa)/2
+  out <- data.frame(grp1 = character(total_comp), 
+                    grp2 = character(total_comp),
+                    comp = character(total_comp),
+                    type = rep("inter-group", total_comp),
+                    mu = numeric(total_comp), 
+                    sd = numeric(total_comp), 
+                    min_dist = numeric(total_comp),
+                    max_dist = numeric(total_comp), stringsAsFactors = F)
+  n_comp = 1
+  for(i in 1:n_taxa){
+    g1 <- taxa[i]
+    seq_1 <- as.character(categories[categories$groups == g1, 'seq_id'])
+    for(j in i:n_taxa){
+      g2 <- taxa[j]
+      seq_2 <- as.character(categories[categories$groups == g2, 'seq_id'])
+      tmp_dat <- dat[seq_1, seq_2]
+      if(i == j) {
+        tmp_dat <- tmp_dat[lower.tri(tmp_dat)]
+        out[n_comp, 'type'] <- 'intra-group'
+      }
+      out[n_comp, "grp1"] <- g1
+      out[n_comp, "grp2"] <- g2
+      out[n_comp, "comp"] <- paste(g1, g2, sep='_')
+      out[n_comp, "mu"] <- mean(tmp_dat)
+      out[n_comp, "sd"] <- sd(tmp_dat)
+      out[n_comp, "min_dist"] <- min(tmp_dat)
+      out[n_comp, "max_dist"] <- max(tmp_dat)
+      n_comp = n_comp + 1
+    }
+  }
+  return(out)
+}
+
+################################################################################
+
+
+################################################################################
+# If running from the command line:
+# 
+
+if(!interactive()) {
+  # collect the arguments
+  args <- commandArgs(trailingOnly = TRUE)
+  
+  # check the argument length
+  if(length(args) < 1 | length(args) > 3) {
+    args <- c("--help")
+  }
+  
+  ## Help section
+  if("--help" %in% args) {
+    cat("
+        Summarise SNP differences
+        
+        Arguments:
+        filename    - a string defining the path to the categories file
+        --seq=filename    - a string defining the path to
+                            the FASTA file <optional>
+        --diff=filename   - a string defining the path to 
+                            the SNP differences matrix <optional>
+        --help            - print this text
+        
+        Example:
+        ./summarise_snp_differences.R \"/home/user/cat.csv\" --seq=\"/home/user/seq.fa\"\n\n")
+    
+    q(save="no")
+  }
+  
+  cat(args, "\n")
+  
+  cat_file = args[1]
+  
+  args <- args[2:length(args)]
+  
+  parse_args <- function(x) strsplit(sub("^--", "", x), "=")
+  args_df <- as.data.frame(do.call("rbind", parse_args(args)), stringsAsFactors = FALSE)
+  names(args_df) <- c("key", "value")
+  
+  if('diff' %in% args_df$key) {
+    diff_file = args_df[args_df$key == 'diff', 'value']
+  } else {
+    seq_file = args_df[args_df$key == 'seq', 'value']
+  }
+}
+
+cat(cat_file, seq_file, diff_file, "\n")
 
